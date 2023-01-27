add4f71517c0d20e15c7972fa8730cc15a2f5f4a
Zach Thomson <zachthomson6@Zachs-Mac.local>
2023-01-27 12:55:37 -0500

    Made code more modular

--- dosing.py
+++ dosing.py
@@ -7,7 +7,7 @@ pediatric
 """
 
 
-def dose_amount():
+def diagnosis():
     print("Day One Dosing Guidelines")
     print("")
     print("Choose diagnosis:")
@@ -16,18 +16,30 @@ def dose_amount():
     print("3 - Community-acquired pneumonia")
     print("4 - Pharyngitis/tonsilitis")
     diagnosis = int(input("Enter a number: "))
+    return diagnosis
+
+def weight_input():
     print("PATIENT WEIGHT")
     print("Enter patient weight followed by units of kg or lb.")
     print("Examples:  65.3 lb      21.0 kg")
     weight_input = input("Enter weight: ")
+    return weight_input
+
+def weight():
     weight_data = weight_input.split(" ")
     weight = float(weight_data[0])
     units = weight_data[1]
     if units == "lb":
         weight = weight / 2.205
+    return weight
+
+def dosage_calc():
     dosages_mg_per_kg = [30, 10, 10, 12]
     dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
     dosage_mg_first_day = weight * dosage_mg_per_kg
+    return dosage_mg_first_day
+
+def dosage():
     print("CORRECT DOSAGE")
     print("For a patient weighing {:.1f} kg,".format(weight))
     print("  the correct dosage is {:.1f} mg the first day"
@@ -35,4 +47,7 @@ def dose_amount():
 
 
 if __name__ == '__main__':
-    dose_amount()
+    diag = diagnosis()
+    weight_text = weight(weight_text)
+    dosage_calc()
+    dosage()
