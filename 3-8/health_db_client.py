48fa4811aea8f6a6a4e584df41bbbd024b7fcbc5
Zach Thomson <zachthomson6@Zachs-Mac.local>
2023-03-10 16:09:43 -0500

    Added classwork from 3-10

--- 3-8/health_db_client.py
+++ 3-8/health_db_client.py
@@ -1,19 +1,26 @@
 import requests
 
-server = "http://127.0.0.1:5000"
 
-patient = {"id": 1, "name": "David", "blood_type": "O+"}
-r = requests.post(server + "/new_patient", json=patient)
-print(r.status_code)
-print(r.text)
+if __name__ == "__main__":
+    server = "http://127.0.0.1:5000"
 
-patient = {"mrn": 2, "name": "John", "blood_type": "A-"}
-r = requests.post(server + "/new_patient", json=patient)
-print(r.status_code)
-print(r.text)
+    patient = {"id": 1, "name": "David", "blood_type": "O+"}
+    r = requests.post(server + "/new_patient", json=patient)
+    print(r.status_code)
+    print(r.text)
+
+    patient = {"id": 2, "name": "John", "blood_type": "A-"}
+    r = requests.post(server + "/new_patient", json=patient)
+    print(r.status_code)
+    print(r.text)
 
 
-data = {"id": 1, "test_name": "HDL", "test_result": 150}
-r = requests.post(server + "/add_test", json=data)
-print(r.status_code)
-print(r.text)
+    data = {"id": 1, "test_name": "HDL", "test_result": 150}
+    r = requests.post(server + "/add_test", json=data)
+    print(r.status_code)
+    print(r.text)
+
+
+    r = requests.get(server + "/get_results/1")
+    print(r.status_code)
+    print(r.text)
